951f285a5f950d8573ea10604c5f7445cc7c0eaa
yuusyaasisutanto <11228403+yuusyaasisutanto@users.noreply.github.com>
2025-07-17 01:26:08 +0900

    Setup ModID and more
    also namespace

--- src/main/java/com/yuusyaasisutanto/bringme115/BringMe115.java
+++ src/main/java/com/yuusyaasisutanto/bringme115/BringMe115.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.yuusyaasisutanto.bringme115;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -29,11 +29,11 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(BringMe115.MODID)
+public class BringMe115
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    public static final String MODID = "bringme115";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -60,7 +60,7 @@ public class ExampleMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod(FMLJavaModLoadingContext context)
+    public BringMe115(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
 
