80f53373baead44cba7bef939dcda7790004a6bf
yuusyaasisutanto <11228403+yuusyaasisutanto@users.noreply.github.com>
2026-02-20 01:00:46 +0900

    Studing will continue...
    一先ずGeminiの活用を開始した
    Vibeじゃなくて、自分のスキル向上を目的としてどこがどういう機能を持つかを学びつつ、別所の資料を見つつで実装を行う。
    
    更新内容としてはmixinにてメンバ変数とセッターメソッドを設定したのみ。

--- src/main/java/com/yuusyaasisutanto/bringme115/mixin/tacz/BM115EntityKineticBullet.java
+++ src/main/java/com/yuusyaasisutanto/bringme115/mixin/tacz/BM115EntityKineticBullet.java
@@ -2,14 +2,25 @@ package com.yuusyaasisutanto.bringme115.mixin.tacz;
 
 import com.tacz.guns.entity.EntityKineticBullet;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(EntityKineticBullet.class)
 public class BM115EntityKineticBullet {
+/*
+
+*/
+    @Unique
+    private float bring_Me_115_code$bm115IntegrateDamageModifier = 1.0f;
+
+    @Unique
+    public void setBm115IntegrateDamageModifier(float bm115IntegrateDamageModifier) {
+        this.bring_Me_115_code$bm115IntegrateDamageModifier = bm115IntegrateDamageModifier;
+    }
 
     @ModifyArg(method = "getDamage", at = @At(value = "INVOKE", target = "Ljava/lang/Math;max(FF)F"), index = 0, remap = false)
     public float apply115InfusedDamage(float original){
-        return original * 100F;
+        return original * this.bring_Me_115_code$bm115IntegrateDamageModifier;
     }
 }
