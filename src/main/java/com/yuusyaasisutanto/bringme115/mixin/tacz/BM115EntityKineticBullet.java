94ac9c4d8e37fe522c8ffbfb6840fde2e3d590be
yuusyaasisutanto <11228403+yuusyaasisutanto@users.noreply.github.com>
2026-02-22 04:47:34 +0900

    PaP機能根幹部分の実装完了
    ありがとうバリン鯖、ありがとう歴戦のmodder
    しかしまだ実装しないといけない機能は多く壁は高い

--- src/main/java/com/yuusyaasisutanto/bringme115/mixin/tacz/BM115EntityKineticBullet.java
+++ src/main/java/com/yuusyaasisutanto/bringme115/mixin/tacz/BM115EntityKineticBullet.java
@@ -1,17 +1,31 @@
 package com.yuusyaasisutanto.bringme115.mixin.tacz;
 
 import com.tacz.guns.entity.EntityKineticBullet;
+import com.tacz.guns.resource.pojo.data.gun.BulletData;
+import com.tacz.guns.resource.pojo.data.gun.GunData;
 import com.yuusyaasisutanto.bringme115.content.pap.BM115LevelToDamage;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import org.checkerframework.checker.units.qual.A;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityKineticBullet.class)
-public class BM115EntityKineticBullet {
-/*
+public abstract class BM115EntityKineticBullet {
+
+
+    /*
 
 */
     @Unique
@@ -23,14 +37,22 @@ public class BM115EntityKineticBullet {
     }
 
     @Unique
-    public float getBm115PapDamageModifier(ItemStack targetGunItem){
+    public int getBm115PapDamageModifier(ItemStack targetGunItem){
         CompoundTag nbt = targetGunItem.getTag();
         if(nbt != null && nbt.contains("BM115Modify")){
             CompoundTag BM115Modify = nbt.getCompound("BM115Modify");
-            return BM115Modify.getInt("PaPLvL");
+            return BM115Modify.getInt("PaPlvl");
         }
         // PAPレベル非適用時は0を返す
         return 0;
+    }
+
+    @Inject(
+            method = "Lcom/tacz/guns/entity/EntityKineticBullet;<init>(Lnet/minecraft/world/entity/EntityType;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/resources/ResourceLocation;Lnet/minecraft/resources/ResourceLocation;Lnet/minecraft/resources/ResourceLocation;ZLcom/tacz/guns/resource/pojo/data/gun/GunData;Lcom/tacz/guns/resource/pojo/data/gun/BulletData;)V",
+            at = @At(value = "TAIL"), remap = false)
+    public void setBm115PapLevelToEntityKineticBullet(EntityType type, Level worldIn, LivingEntity throwerIn, ItemStack gunItem, ResourceLocation ammoId, ResourceLocation gunId, ResourceLocation gunDisplayId, boolean isTracerAmmo, GunData gunData, BulletData bulletData, CallbackInfo ci){
+        this.bring_Me_115_code$bm115PapDamageModifier = BM115LevelToDamage.getPackAPunchedDamage(getBm115PapDamageModifier(gunItem));
+        //                bring_Me_115_code$bm115PapDamageModifier = setBm115PapDamageModifier(getBm115PapDamageModifier(gunItem));
     }
 
     @ModifyArg(method = "getDamage", at = @At(value = "INVOKE", target = "Ljava/lang/Math;max(FF)F"), index = 0, remap = false)
--- src/main/java/com/yuusyaasisutanto/bringme115/mixin/tacz/BM115ModernKineticGunScriptAPI.java
+++ src/main/java/com/yuusyaasisutanto/bringme115/mixin/tacz/BM115ModernKineticGunScriptAPI.java
@@ -1,10 +1,24 @@
 package com.yuusyaasisutanto.bringme115.mixin.tacz;
 
+import com.tacz.guns.api.util.LuaNbtAccessor;
+import com.tacz.guns.entity.EntityKineticBullet;
 import com.tacz.guns.item.ModernKineticGunScriptAPI;
+import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(ModernKineticGunScriptAPI.class)
-public class BM115ModernKineticGunScriptAPI {
-    //次回はここの「銃弾スポーン時に倍率を設定させる」ところから
+public abstract class BM115ModernKineticGunScriptAPI {
+/*    @Shadow public abstract LuaNbtAccessor getNbt();
 
+    //次回はここの「銃弾スポーン時に倍率を設定させる」ところから
+    @ModifyArg(method = "lambda$shootOnce$2",at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;addFreshEntity(Lnet/minecraft/world/entity/Entity;)Z"), remap = false)
+    public Entity setBM115DamageModifiersToEntity(Entity original){
+            original.setB
+        return original;
+
+    }
+*/
 }
