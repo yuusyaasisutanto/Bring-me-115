7a4fcce92461791df4f79aa6567a8aca0910f122
yuusyaasisutanto <11228403+yuusyaasisutanto@users.noreply.github.com>
2026-06-01 04:47:28 +0900

    Datagenいじり
    めんどいので少しずつ、タグの登録を完了したが、まだHQには渡してないのでこのままだとDatagen時に走らない

--- src/main/java/com/yuusyaasisutanto/bringme115/content/datagen/subdivide/BM115BlockTagGenerator.java
+++ src/main/java/com/yuusyaasisutanto/bringme115/content/datagen/subdivide/BM115BlockTagGenerator.java
@@ -1,4 +1,48 @@
 package com.yuusyaasisutanto.bringme115.content.datagen.subdivide;
 
-public class BM115BlockTagGenerator {
+import com.yuusyaasisutanto.bringme115.BringMe115;
+import com.yuusyaasisutanto.bringme115.content.register.BM115BlockRegister;
+import com.yuusyaasisutanto.bringme115.content.register.BM115ItemRegister;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class BM115BlockTagGenerator extends BlockTagsProvider {
+    public BM115BlockTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, String modId, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, BringMe115.ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+        // ピッケルで採集可能
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(
+                        BM115BlockRegister.RAW_ELEMENT115_BLOCK.get(),
+                        BM115BlockRegister.DEEPSLATE_ELEMENT115_ORE.get(),
+                        BM115BlockRegister.PRIMITIVE_MACHINE.get()
+                );
+
+        // ダイヤツールが必須
+        this.tag(BlockTags.NEEDS_DIAMOND_TOOL)
+                .add(
+                        BM115BlockRegister.RAW_ELEMENT115_BLOCK.get(),
+                        BM115BlockRegister.DEEPSLATE_ELEMENT115_ORE.get()
+                );
+
+        // element115鉱石のタグをフォージ側に作成
+        TagKey<Block> tagElement115 = TagKey.create(Registries.BLOCK, new ResourceLocation("forge", "ores/element115"));
+
+        // ↑に登録させる
+        this.tag(tagElement115).add(BM115BlockRegister.DEEPSLATE_ELEMENT115_ORE.get());
+
+    }
 }
--- src/main/java/com/yuusyaasisutanto/bringme115/content/datagen/subdivide/BM115ItemTagGenerator.java
+++ src/main/java/com/yuusyaasisutanto/bringme115/content/datagen/subdivide/BM115ItemTagGenerator.java
@@ -1,4 +1,37 @@
 package com.yuusyaasisutanto.bringme115.content.datagen.subdivide;
 
-public class BM115ItemTagGenerator {
+import com.yuusyaasisutanto.bringme115.BringMe115;
+import com.yuusyaasisutanto.bringme115.content.register.BM115ItemRegister;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class BM115ItemTagGenerator extends ItemTagsProvider {
+    public BM115ItemTagGenerator(PackOutput p_275343_, CompletableFuture<HolderLookup.Provider> p_275729_, CompletableFuture<TagLookup<Block>> p_275322_, String modId, @Nullable ExistingFileHelper existingFileHelper) {
+        super(p_275343_, p_275729_, p_275322_, BringMe115.ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+
+        // Block側で作成したタグをItem側にコピー
+        TagKey<Block> blockForgeTag = TagKey.create(Registries.BLOCK, new ResourceLocation("forge", "ores/element115"));
+        TagKey<Item> itemForgeTag = TagKey.create(Registries.ITEM, new ResourceLocation("forge", "ores/element115"));
+
+        this.copy(blockForgeTag, itemForgeTag);
+
+        // raw element115用タグ
+        TagKey<Item> rawMaterialElement115 = TagKey.create(Registries.ITEM, new ResourceLocation("forge", "raw_materials/element115"));
+
+        this.tag(rawMaterialElement115).add(BM115ItemRegister.RAW_ELEMENT115.get());
+    }
 }
