46dd5939d955f00b2d2b15d92ba2aea4631e41bb
yuusyaasisutanto <11228403+yuusyaasisutanto@users.noreply.github.com>
2026-06-02 00:13:14 +0900

    おそらくLootのDatagenが完了
    まだ走らせてないからわからんが

--- src/main/java/com/yuusyaasisutanto/bringme115/content/datagen/subdivide/BM115LootTableGenerator.java
+++ src/main/java/com/yuusyaasisutanto/bringme115/content/datagen/subdivide/BM115LootTableGenerator.java
@@ -1,4 +1,23 @@
 package com.yuusyaasisutanto.bringme115.content.datagen.subdivide;
 
+import com.yuusyaasisutanto.bringme115.content.datagen.subdivide.loot.BM115BlockLootTables;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+
+import java.util.List;
+import java.util.Set;
+
 public class BM115LootTableGenerator {
+    public static LootTableProvider create(PackOutput output){
+            return new LootTableProvider(
+                    output,
+                    Set.of(),
+                    List.of(new LootTableProvider.SubProviderEntry(
+                            BM115BlockLootTables::new,
+                            LootContextParamSets.BLOCK
+                    ))
+            );
+    };
 }
--- src/main/java/com/yuusyaasisutanto/bringme115/content/datagen/subdivide/loot/BM115BlockLootTables.java
+++ src/main/java/com/yuusyaasisutanto/bringme115/content/datagen/subdivide/loot/BM115BlockLootTables.java
@@ -1,4 +1,52 @@
 package com.yuusyaasisutanto.bringme115.content.datagen.subdivide.loot;
 
-public class BM115BlockLootTables {
+import com.yuusyaasisutanto.bringme115.content.register.BM115BlockRegister;
+import com.yuusyaasisutanto.bringme115.content.register.BM115ItemRegister;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class BM115BlockLootTables extends BlockLootSubProvider {
+
+    public BM115BlockLootTables() {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags());
+    }
+
+    @Override
+    protected void generate() {
+        // そのものをドロップさせる
+        this.dropSelf(BM115BlockRegister.PRIMITIVE_MACHINE.get());
+        this.dropSelf(BM115BlockRegister.RAW_ELEMENT115_BLOCK.get());
+
+        // 鉱石
+        this.add(BM115BlockRegister.DEEPSLATE_ELEMENT115_ORE.get(),
+                block -> createRawOreDrops(BM115BlockRegister.DEEPSLATE_ELEMENT115_ORE.get(),
+                        BM115ItemRegister.RAW_ELEMENT115.get()));
+
+    }
+
+    protected LootTable.Builder createRawOreDrops(Block block, Item item){
+        return createSilkTouchDispatchTable(block,
+                    this.applyExplosionDecay(block,
+                            LootItem.lootTableItem(item)
+                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(2.0F, 4.0F)))
+                                .apply(ApplyBonusCount.addOreBonusCount(Enchantments.BLOCK_FORTUNE))
+                ));
+    }
+
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return BM115BlockRegister.REGISTRY.getEntries().stream().map(RegistryObject::get).collect(Collectors.toList());
+    }
 }
