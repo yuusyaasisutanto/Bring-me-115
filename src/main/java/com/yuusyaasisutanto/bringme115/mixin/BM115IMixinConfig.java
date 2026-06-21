337b9296be6774c99282d7b7313496767d4bcf16
yuusyaasisutanto <11228403+yuusyaasisutanto@users.noreply.github.com>
2026-06-22 01:18:40 +0900

    未動作:Presenceの動的mixinの挑戦
    modの有無によってmixinできるように組んだつもりが最初の1回は動いていろいろといじくってるうちに実践環境で動かなかったり開発環境でも動かなくなったりしている

---
+++ src/main/java/com/yuusyaasisutanto/bringme115/mixin/BM115IMixinConfig.java
@@ -0,0 +1,49 @@
+package com.yuusyaasisutanto.bringme115.mixin;
+
+import net.minecraftforge.fml.ModList;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class BM115IMixinConfig implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return "";
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (mixinClassName.equals("com.yuusyaasisutanto.bringme115.mixin.tacz_presence.BM115ImmersiveGunHudOverlay")){
+            return ModList.get().isLoaded("tacz_presence");
+        }
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return List.of();
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+}
